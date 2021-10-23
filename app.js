ad1e776d16a846ed0c65326b4664c6d14224d990
Luis Tripa <54379665+luistripa@users.noreply.github.com>
2021-10-23 12:40:15 +0100

    Added comments

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const ANGULAR_VELOCITY = 0.01;
 /** @type {WebGLRenderingContext} */
 let gl;
 
+// Program1 - Responsible for the drawing of the grid and electric field lines
+// Program2 - Responsible for the drawing of the stylization of the charges. + for positive and - for negative
 var program1, program2, canvas;
 
 let aBuffer, bBuffer;
@@ -16,13 +18,19 @@ const table_width = 3.0;
 let table_height;
 
 let vertices = [];
-let position = []; // Array que guarda as cargas elétricas
-let chargeValue = [];
+let position = []; // Array that holds all the charges
+let chargeValue = []; // Arrays that holds the values of the charges contained in position in the same order
 
-let drawCharges = true;
+let drawCharges = true; // Boolean value to indicate if the charge's stylization should be drawn.
 
-const grid_spacing = 0.05;
+const grid_spacing = 0.05; // The space between each point in the grid
 
+/*
+Called every refresh cycle.
+
+Draws the entire grid and electric fields.
+If drawCharges is true, it also draws a stylized visualization of the charges.
+*/
 function animate(time) {
     window.requestAnimationFrame(animate);
     
@@ -39,6 +47,9 @@ function animate(time) {
     }
 }
 
+/*
+Sets up the entire environment.
+*/
 function setup(shaders) {
     canvas = document.getElementById("gl-canvas");
     canvas.width = window.innerWidth;
@@ -53,10 +64,13 @@ function setup(shaders) {
 
     for (let x = -table_width/2.0+grid_spacing/2; x < table_width/2.0+grid_spacing/2; x+=grid_spacing) {
         for (let y=-table_height/2.0+grid_spacing/2; y < table_height/2.0+grid_spacing/2; y+=grid_spacing) {
+            // Adds "noise" to the points to avoid a perfect grid
             let randomX = x + (Math.random() - 0.5) / 20;
             let randomY = y + (Math.random() - 0.5) / 20;
-            vertices.push(MV.vec3(randomX, randomY, 1.0)); //Movivel
-            vertices.push(MV.vec3(randomX, randomY, 0.0)); 
+
+            // We'll use the z coordintate to distinguish between fixed vertices and movable vertices
+            vertices.push(MV.vec3(randomX, randomY, 1.0)); // Movable vertex
+            vertices.push(MV.vec3(randomX, randomY, 0.0)); // Fixed vertex
         }
     }
 
@@ -66,17 +80,16 @@ function setup(shaders) {
     aBuffer = gl.createBuffer();
     update_program1();
     gl.bufferData(gl.ARRAY_BUFFER, MV.flatten(vertices), gl.STATIC_DRAW);
-
-    update_table(program1);
+    
+    update_table(program1); // Updates uniform values in program 1
 
     /*
     CHARGES DRAWING
     */
     bBuffer = gl.createBuffer();
     update_program2();
-    gl.bufferData(gl.ARRAY_BUFFER, MV.flatten(position.concat(chargeValue)), gl.STATIC_DRAW);
 
-    update_table(program2);
+    update_table(program2); // Updates table_width and table_height in program 2
 
     gl.viewport(0, 0, canvas.width, canvas.height);
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
@@ -104,7 +117,7 @@ function setup(shaders) {
         const transformed_y = -(y * table_height)/canvas.height + table_height/2
         
         if (position.length < MAX_CHARGES) {
-            // Push the new vertex to the vertex array
+            // Push the new charge position into the charges array
             position.push(MV.vec2(transformed_x, transformed_y));
             if(event.shiftKey){
                 chargeValue.push(-CHARGE);
@@ -118,6 +131,11 @@ function setup(shaders) {
         }
     });
 
+    /*
+    This event handler turn on/off the visualization of stylized charges.
+
+    Press space to turn on/off
+    */
     window.addEventListener("keydown", function(event){
         if(event.key == " "){
             drawCharges = !drawCharges;
@@ -126,7 +144,7 @@ function setup(shaders) {
 }
 
 /**
- * Sends the vertex array to the vertex shader as uniform variables.
+ * Sends the charges array to the vertex shader of program 2.
  */
 function update_charges() {
     gl.useProgram(program1);
@@ -148,9 +166,12 @@ function update_charges() {
     gl.bufferData(gl.ARRAY_BUFFER, new_charges, gl.STATIC_DRAW);
 }
 
+/*
+Rotates all charges around the center of the canvas.
+*/
 function rotate_charges(){
     for(let i = 0; i < position.length; i++){
-        let distance = Math.sqrt(Math.pow(position[i][0], 2) + Math.pow(position[i][1], 2)); //Distancia a origem
+        let distance = Math.sqrt(Math.pow(position[i][0], 2) + Math.pow(position[i][1], 2)); // Distance to center of canvas
         let alpha = Math.atan2(position[i][1], position[i][0]);
         if(chargeValue[i] > 0){ 
             alpha += ANGULAR_VELOCITY;
@@ -163,6 +184,9 @@ function rotate_charges(){
     update_charges();
 }
 
+/*
+Updates table_width and table_height in the given program.
+*/
 function update_table(program){
     gl.useProgram(program);
     let t_width_position = gl.getUniformLocation(program, "table_width");
@@ -171,14 +195,20 @@ function update_table(program){
     gl.uniform1f(t_height_position, table_height);
 }
 
+/**
+ * Updates the attribute positions of program 1.
+ */
 function update_program1(){
     gl.useProgram(program1);
     gl.bindBuffer(gl.ARRAY_BUFFER, aBuffer);
     let vPosition = gl.getAttribLocation(program1, 'vPosition');
-    gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 0, 0);
+    gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 0, 0); // 3 floats... remmember we use the last coord to distinguish between fixed and movable.
     gl.enableVertexAttribArray(vPosition);
 }
 
+/*
+Updates the attribute positions of program 2.
+*/
 function update_program2(){
     gl.useProgram(program2);
     gl.bindBuffer(gl.ARRAY_BUFFER, bBuffer);
